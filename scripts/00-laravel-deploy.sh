47f04dc0f46ca0205beae1999251bf3d0c74e1e1
Nama3b <leethanhlong7890@gmail.com>
2025-02-18 10:07:38 +0700

    update laravel deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ set -o errexit
 #!/usr/bin/env bash
 echo "Running composer"
 composer clear-cache
-composer install --no-dev --optimize-autoloader --prefer-dist
+composer install --no-dev --optimize-autoloader --prefer-source
 
 npm install
 npm run dev
